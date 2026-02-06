e97441758db0efc850a87babf37c5feb778ec5ed
harisankarmh <hari@octopix.net>
2026-02-06 12:19:32 +0530

    Members Export Update

--- web/evgadmin/actions/anyadmin/methods/members-export.php
+++ web/evgadmin/actions/anyadmin/methods/members-export.php
@@ -391,11 +391,14 @@ if (!$pix->canAccess('members') || !$pix->canAccess('elect')) {
         }
     }
     $members = [];
-    $whereClause = '';
+    $whereClause = 'WHERE m.enabled = "Y"';
 
     if ($idString) {
-        $whereClause = ' WHERE m.id IN (' . $idString . ')';
+        $whereClause = ' AND m.id IN (' . $idString . ')';
     }
+
+    var_dump($whereClause);
+    exit;
     $members = $pixdb->fetchAll(
         "SELECT 
             i.prefix,
